88d5b10af421bca97cc8ad7aff9a369ebf33ab3b
Aryan9901 <911aaryan@gmail.com>
2024-06-06 11:55:39 +0530

    team changes

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,4 +1,3 @@
-
 import { AdminSidebar, Bar, Loader, OwnerRow, Table, TableBody, TableContainer, TableHeaders, TableHeading, TextLoader } from "../components";
 import Select, { components } from "react-select";
 import { LineChart } from "../components/Chart";
@@ -7,8 +6,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTeam } from "../redux/actions";
 
-
-const teamGroupHeaders = ["#", "Name", "Rank", "Total Income", "Address"];
+const teamGroupHeaders = ["S No", "Name", "Rank", "Total Income", "Address"];
 
 const teamTopHeaders = ["S.No", "Name", "Rank", "Total Income", "Address"];
 
@@ -80,6 +78,31 @@ const DropdownIndicator = (props) => {
 	);
 };
 
+const levelMap = {
+	0: "JOINING",
+	1: "JOINING",
+	2: "JOINING",
+	3: "JOINING",
+	4: "JOINING",
+	5: "JOINING",
+	6: "JOINING",
+	7: "JOINING",
+	8: "JOINING",
+	9: "JOINING",
+	10: "SILVER",
+	11: "GOLD",
+	12: "STARGOLD",
+	13: "PLATINUM",
+	14: "EMRALD",
+	15: "RUBI",
+	16: "DIAMOND",
+	17: "DOUBLE DIAMOND",
+	18: "STAR DIAMOND",
+	19: "CROWN",
+	20: "STAR CROWN",
+	21: "DOUBLE CROWN",
+};
+
 const Profile = () => {
 	const { teams, loading } = useSelector((state) => state.team);
 	const { user, users } = useSelector((state) => state.user);
@@ -100,7 +123,7 @@ const Profile = () => {
 						<p>{top.rank}</p>
 					</div>
 				</div>
-				<div className="price">{top?.totalBonus}</div>
+				<div className="price">{top?.referralIncome}</div>
 			</div>
 		));
 	}, [topPerformer]);
@@ -115,19 +138,20 @@ const Profile = () => {
 
 	useEffect(() => {
 		if (teams) {
-			const sortedTeams = [...teams].sort((a, b) => b.totalBonus - a.totalBonus);
+			// console.log(teams);
+			const sortedTeams = [...teams].sort((a, b) => b.referralIncome - a.referralIncome);
 			const topPerformers = sortedTeams.slice(0, 4);
 			const topData = topPerformers.map((top) => ({
 				_id: top._id,
 				name: top.firstName + " " + top.lastName,
 				rank: top.rank,
-				totalBonus: top.totalBonus,
+				referralIncome: top.referralIncome,
 			}));
 			// console.log(topData);
 			setTopPerformer(topData);
 			const data = sortedTeams.map((member, idx) => {
 				return {
-					data: [idx + 1, member.firstName + " " + member.lastName, member.rank, member.totalBonus, member.state],
+					data: [idx + 1, member.firstName + " " + member.lastName, levelMap[member.level], member.referralIncome, member.state],
 					_id: member._id,
 				};
 			});
@@ -174,9 +198,8 @@ const Profile = () => {
 				<Bar heading="Team" />
 				<section className="team">
 					{isAdmin === "admin" && (
-						<div className="team__header" >
+						<div className="team__header">
 							<Select
-							
 								styles={customStyles}
 								components={{ DropdownIndicator }}
 								options={teamOptions}
@@ -220,19 +243,8 @@ const Profile = () => {
 					<div className="team__footer">
 						<div className="top_performer">
 							<div className="heading">Top Performers</div>
-							<div className="table-performer">
-								{loading ? <TextLoader /> : memoizedPerformers}
-							</div>
+							<div className="table-performer">{loading ? <TextLoader /> : memoizedPerformers}</div>
 						</div>
-						{/* <div className="group_performance">
-							<h1>Group Performace</h1>
-							<LineChart
-								backgroundColor="#90A9C3"
-								borderColor="#003D79"
-								data={[12, 23, 23, 564, 342, 243, 3, 556, 34, 76, 32, 345]}
-								labels={["jan", "feb", "mar", "apr", "may", "june", "july", "aug", "sept", "oct", "nov", "dec"]}
-							/>
-						</div> */}
 					</div>
 				</section>
 			</main>
